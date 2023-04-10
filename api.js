658ec8d93eb83f53f0d20eb5c15b4bed20b06e9f
hao-beixi <87632566+hao-beixi@users.noreply.github.com>
2023-04-09 20:31:07 -0400

    add function to randomly create a user

--- api.js
+++ api.js
@@ -1,1 +1,18 @@
-
+function createUser() {
+    const animals = ['cat', 'dog', 'hamster', 'rabbit', 'parrot'];
+    const randomIndex = Math.floor(Math.random() * animals.length);
+    const animalName = animals[randomIndex];
+    const randomNum = Math.floor(Math.random() * 1000);
+    const username = `${animalName}${randomNum}`;
+    
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    users.push(username);
+    localStorage.setItem('users', JSON.stringify(users));
+    
+    return username;
+  }
+  
+  module.exports = {
+    createUser
+  };
+  
